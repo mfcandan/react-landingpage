3b452d40057a5ada900aed254821fd6c8133e5b5
Muhammed Fatih Candan <mfatihcandan@gmail.com>
2020-10-18 14:20:11 +0300

    Button & navbar create

--- react-responsive-site/src/App.js
+++ react-responsive-site/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import {BrowserRouter as Router, Swithc, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />  
+        <Switch>
+          <Route path="/" exact />
+        </Switch>
       </Router>
     </div>
   );
