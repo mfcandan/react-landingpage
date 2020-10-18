3c235b791f46bcc86b3a400c519199cc3d9d1f44
Muhammed Fatih Candan <mfatihcandan@gmail.com>
2020-10-18 11:29:45 +0300

    font awesome

--- react-responsive-site/src/components/Navbar.js
+++ react-responsive-site/src/components/Navbar.js
@@ -5,7 +5,9 @@ function Navbar() {
         <>
             <nav className="navbar">
                 <div className="navbar-container">
-                        
+                    <Link to="/" className="navbar-logo">
+                        TRVL
+                    </Link>
                 </div>
             </nav> 
         </>
