b2a55c9798ac73642122fd41ae762b04014eefce
Muhammed Fatih Candan <mfatihcandan@gmail.com>
2020-10-18 13:07:39 +0300

    navbar

--- react-responsive-site/src/App.js
+++ react-responsive-site/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Swithc, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h1>Yo</h1>  
+      <Router>
+        <Navbar />  
+      </Router>
     </div>
   );
 }
