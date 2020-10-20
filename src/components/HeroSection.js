5c8addf23ef7feee1a37e3b9fc93beb01b8b6139
Muhammed Fatih Candan <mfatihcandan@gmail.com>
2020-10-20 09:18:09 +0300

    welcome text

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,7 +7,7 @@ function HeroSection() {
     return (
         <div className="hero-container">
             <video src="/videos/video-2.mp4" autoPlay loop muted />
-            <h1>Adventure Awaits</h1>
+            <h1>Welcome to Portfolio Site</h1>
             <p>What are you waiting</p>
             <div className="hero-btns">
                 <Button 
